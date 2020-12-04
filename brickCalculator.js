d1e062e825662fd77bba525ad35758ee58e88741
firoz82 <ovrohossain763@gmail.com>
2020-12-04 12:30:09 +0600

    first commit

---
+++ brickCalculator.js
@@ -0,0 +1,20 @@
+  
+function brickCalculator(tala) {
+    if (tala <= 10) {
+        var Eit = (tala * 15 * 1000);
+        return Eit;
+    }
+
+    else if (tala > 10 && tala < 20) {
+        var Eit = (10 * 15 * 1000) + (tala - 10) * 12 * 1000;
+    return Eit;
+}
+
+    else if (tala => 20) {
+    var Eit = (10 * 15 * 1000) + (10 * 12 * 1000) + (tala - 20) * 10 * 1000;
+return Eit;
+    }
+}
+
+var result = brickCalculator(22)
+console.log("Total Eit Needs : " +result);
